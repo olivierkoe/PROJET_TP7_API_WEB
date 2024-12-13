a609c9443049896b0f7e6021377db32c9983af6e
dustyrix <godineau.pro@gmail.com>
2024-12-13 17:09:39 +0100

    add started template

--- src/router/clients_router.py
+++ src/router/clients_router.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
 
+#Create = POST
+#Read = GET
+#Update = PUT
+#Delete = DELETE
 
 router_client = APIRouter()
 
+
 @router_client.get("/clients")
+async def get_clients():
+    return [{"message": "Liste des clients"}]
+
+@router_client.post("/clients")
 async def get_clients():
     return [{"message": "Liste des clients"}]
