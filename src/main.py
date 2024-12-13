d0554d4870b14c214b2f5ede1fd9e880cdddbe32
dustyrix <godineau.pro@gmail.com>
2024-12-13 14:55:30 +0100

    maj main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
-# importer fastapi
 from fastapi import FastAPI
-from src.database import engine
-from src.models import Base
 
-# definir les routes
 app = FastAPI()
 
-# importer engine(database.py) et Base(models.py)
-Base.metadata.create_all(engine)
-
-
 @app.get("/")
-def toto():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
